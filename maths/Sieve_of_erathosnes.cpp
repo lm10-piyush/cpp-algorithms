1e61e660865ac261b7325f3c7aa8004067d3f074
LM10Piyush <40412144+LM10Piyush@users.noreply.github.com>
2019-09-16 23:38:10 +0530

    update the file and file name
    update the file and file name

--- maths/Sieve_of_erathosnes.cpp
+++ maths/Sieve_of_erathosnes.cpp
@@ -24,24 +24,8 @@ void sieve(bool A[],int n){
 }
  
 int main(){
-	int x=1000000000;
-	int n=x;
-	long long int data = 999999937;
-	long long int max = data*data,max2 = (data-1)*(data-1);
-	bool *A = new bool[x+1];
-	sieve(A,x);
-	for(long long int i=2;i<n;i++){
-		if(A[i]==true){
-		long long int mod=  max%i;
-		max2 = (data-mod*mod)*(data-mod*mod);
-		for(long long int j=2;j<n;j++){
-			if(A[j]==true){
-				if(max2%j==mod){
-					cout<<"("<<i<<","<<j<<")"<<mod<<"  ";
-				}
-			}
-		}
-	}
-}
-cout<<"Hola";
+	int n=100000;
+	bool *A = new bool[n+1];
+	sieve(A,n);
+	
 }
